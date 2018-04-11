7a4b411bf295acb4dc1a19c4c14b4d9d86723eba
m4rg4sh <epprest1@students.zhaw.ch>
2018-04-11 19:37:56 +0200

    Adapted for BrownBagToken Demo

--- custom.js
+++ custom.js
@@ -1,36 +1,74 @@
 $(window).on('load', function() {
     
-    var contractAddress = "0x563b01e18316d1e85c320335b1360c25648af12d"; // in Ropsten testnet!
+    var contractAddress = "0xca7750fb64199206274e9fd004f4eb00f3f01776"; // in Ropsten testnet!
     var contractAbi = [
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "getGreeting",
-		"outputs": [
-			{
-				"name": "s",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "s",
-				"type": "string"
-			}
-		],
-		"name": "setGreeting",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
-];
+        {
+            "constant": false,
+            "inputs": [],
+            "name": "buyToken",
+            "outputs": [],
+            "payable": true,
+            "stateMutability": "payable",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [
+                {
+                    "name": "to",
+                    "type": "address"
+                },
+                {
+                    "name": "amount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "transfer",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "constant": true,
+            "inputs": [
+                {
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "name": "balanceOf",
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "owner",
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ];
 
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
     if (typeof web3 !== 'undefined') {
@@ -46,27 +84,41 @@ $(window).on('load', function() {
     
     // create instance of contract object that we use to interface the smart contract
     var contractInstance = web3.eth.contract(contractAbi).at(contractAddress);
-    contractInstance.getGreeting(function(error, greeting) {
+    contractInstance.balanceOf(web3.eth.defaultAccount, function(error, balance) {
         if (error) {
-            var errorMsg = 'error reading greeting from smart contract: ' + error;
+            var errorMsg = 'error reading balance from smart contract: ' + error;
             $('#content').text(errorMsg);
             console.log(errorMsg);
             return;
         }
-        $('#content').text('greeting from contract: ' + greeting);
+        $('#content').text('Amount of Tokens: ' + web3.fromWei(balance, 'ether').toString(10));
     });
     
-    $('#my-form').on('submit', function(e) {
+    $('#buyToken').on('submit', function(e) {
         e.preventDefault(); // cancel the actual submit
-        var newGreeting = $('#greeting').val(); 
-        contractInstance.setGreeting(newGreeting, function(error, txHash) {
+        var newAmount = $('#amount').val();
+        contractInstance.buyToken({from: web3.eth.defaultAccount, value: web3.toWei(newAmount, "ether")}, function(error, txHash) {
             if (error) {
-                var errorMsg = 'error writing new greeting to smart contract: ' + error;
+                var errorMsg = 'error buying Token: ' + error;
                 $('#content').text(errorMsg);
                 console.log(errorMsg);
                 return;
             }
-            $('#content').text('submitted new greeting to blockchain, transaction hash: ' + txHash);
+            $('#content').text('sent buying transaction to blockchain, transaction hash: ' + txHash);
+        });
+    });
+
+    $('#transferToken').on('submit', function(e) {
+        e.preventDefault(); // cancel the actual submit
+        var newTransferAmount = $('#transferamount').val();
+        contractInstance.transfer($('#to').val(), web3.toWei(newTransferAmount, "ether"), function(error, txHash) {
+            if (error) {
+                var errorMsg = 'error transfering Token: ' + error;
+                $('#content').text(errorMsg);
+                console.log(errorMsg);
+                return;
+            }
+            $('#content').text('sent transfer transaction to blockchain, transaction hash: ' + txHash);
         });
     });
 
